60f81f9db9bf97552c3e566ffdf83066604effce
Márk Tell <mark.tell@stud.uni-corvinus.hu>
2022-11-04 10:47:13 +0100

    Form1 changes

--- HetedikHet_DX552A/HetedikHet_DX552A/Entities/BallFactory.cs
+++ HetedikHet_DX552A/HetedikHet_DX552A/Entities/BallFactory.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace HetedikHet_DX552A.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
     }
 }
--- HetedikHet_DX552A/HetedikHet_DX552A/Form1.cs
+++ HetedikHet_DX552A/HetedikHet_DX552A/Form1.cs
@@ -12,9 +12,49 @@ namespace HetedikHet_DX552A
 {
     public partial class Form1 : Form
     {
+        private List<Entities.Ball> _balls = new List<Entities.Ball>();
+
+        private Entities.BallFactory _factory;
+
+        public Entities.BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new Entities.BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var b in _balls)
+            {
+                b.MoveBall();
+                if (b.Left > maxPosition)
+                {
+                    maxPosition = b.Left;
+                }
+                if (maxPosition > 1000)
+                {
+                    var oldestBall = _balls[0];
+                    mainPanel.Controls.Remove(oldestBall);
+                    _balls.Remove(oldestBall);
+                }
+            }
         }
     }
 }
